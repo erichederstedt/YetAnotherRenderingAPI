f431120c5237afece2f861fea17da33abc571c8c
Dolf <eric.jonsson.hederstedt@gmail.com>
2025-04-14 05:14:48 +0200

    Rewrote VCVarsFinder into a powershell script

--- Scripts/run_vcvar.bat
+++ Scripts/run_vcvar.bat
@@ -2,12 +2,14 @@
 
 rem Notes:
 rem Only VS2019 and VS2022 has been tested and works.
-rem You cannot have more that one install of a VS version. 
+rem You cannot have more that one install of a VS version.
 rem Such as two installs of VS2022.
 
 pushd %~dp0
 
-for /f "tokens=*" %%a in ('VCVarsFinder.exe') do set filepath=%%a
+for /f "delims=" %%i in ('powershell -nologo -noprofile -executionpolicy bypass -file "VCVarsFinder.ps1"') do (
+    set "filepath=%%i"
+)
 
 echo Executing: "%filepath%"
 
