1f7c2a961fb4d48f386e613089ba186f1896bf52
Dolf <eric.jonsson.hederstedt@gmail.com>
2025-04-14 04:55:57 +0200

    Reapply "Removed old scrap"
    This reverts commit 71856da21916e51970f7c47e988feed0dfd61896.

--- Scripts/debug_run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-pushd %~dp0
-pushd ..
-
-bin\metapts.exe -s Src -f
-
-popd
-popd
--- Src/linker.h
+++ Src/linker.h
@@ -1,5 +1,1 @@
 #pragma comment( lib, "User32.lib" )
-#pragma comment( lib, "d3dcompiler.lib" )
-#pragma comment( lib, "d3d12.lib" )
-#pragma comment( lib, "dxgi.lib" )
-#pragma comment (lib, "dxguid")
